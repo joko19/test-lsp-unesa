8e626b0d6613161af871785bffe9157fc6adc532
joko19 <jokoslamet459@gmail.com>
2022-06-14 21:37:27 +0700

    add image resource

--- pages/home.js
+++ pages/home.js
@@ -3,9 +3,27 @@ import Layout from "../Layout/Layout"
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold text-center underline">
-        Home
-      </h1>
+      <h1 className="text-2xl ">Selamat Datang</h1>
+      <div className="flex gap-4 bg-gray-200 p-4 rounded-lg">
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/persegi.png" />
+        </div>
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/segitiga.png" />
+        </div>
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/lingkaran.png" />
+        </div>
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/kubus.png" />
+        </div>
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/limas.png" />
+        </div>
+        <div className="w-full">
+          <img className="hover:bg-gray-100 cursor-pointer w-24 h-24" src="/images/tabung.png" />
+        </div>
+      </div>
     </Layout>
   )
 }
