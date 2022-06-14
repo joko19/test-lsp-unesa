4a93fc325e3552c4c2910f2ec765b0f0cba40a79
joko19 <jokoslamet459@gmail.com>
2022-06-14 21:12:26 +0700

    add apexchart

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Card from '../components/Cards/Card'
-import Layout from '../Layout/Layout'
-import { IoIosStats } from 'react-icons/io'
-import { IoAlertSharp } from 'react-icons/io5'
 import { useEffect } from 'react'
 
 export default function Home() {
--- pages/statistic.js
+++ pages/statistic.js
@@ -1,11 +1,46 @@
 import Layout from "../Layout/Layout"
+import { useState } from "react";
+import dynamic from 'next/dynamic';
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 export default function Home() {
+  const [productSoldDonut, setproductSoldDonut] = useState({
+    options: {
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+            //   name: {
+            //     ...
+            // },
+            //   value: {
+            //     ...
+            // }
+            }
+          }
+        }
+      }
+    },
+    series: [44, 55, 41],
+    labels: ['Persegi', 'Segitiga', 'Lingkaran'],
+    chartOptions: {
+      labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
+    }
+  })
   return (
     <Layout>
       <h1 className="text-3xl font-bold text-center underline">
         Statistic
       </h1>
+      <div className="flex">
+        <div>
+          <Chart options={productSoldDonut.options} series={productSoldDonut.series} type="donut" width="100%" />
+        </div>
+        <div>
+
+        </div>
+      </div>
     </Layout>
   )
 }
